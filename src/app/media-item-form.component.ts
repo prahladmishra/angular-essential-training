d6d53dae191043876a723f3346bec5d59041b0da
Prahlad Mishra <prahlad.mishra@abbott.com>
2021-08-12 21:36:58 +0100

    end of form module

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
   templateUrl: './media-item-form.component.html',
   styleUrls: ['./media-item-form.component.css']
 })
-export class MediaItemFormComponent {
+export class MediaItemFormComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      medium: new FormControl('Movies'),
+      name: new FormControl('', Validators.compose([
+        Validators.pattern('[\\w\\-\\s\\/]+'),
+        Validators.required
+      ])),
+      category: new FormControl(''),
+      year: new FormControl('', this.yearValidator)
+    });
+  }
+
+  yearValidator(control: FormControl) {
+    if (control.value.trim().length === 0) {
+      return null;
+    }
+    const year = parseInt(control.value, 10);
+    const minYear = 1900;
+    const maxYear = 2100;
+    if (year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return {
+        year: {
+          min: minYear,
+          max: maxYear
+      } };
+    }
+  }
+
   onSubmit(mediaItem) {
     console.log(mediaItem);
   }
