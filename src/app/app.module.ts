df0859b6258c81602b8700e4425dceba0680171a
Prahlad Mishra <prahlad.mishra@abbott.com>
2021-08-13 12:01:43 +0100

    @Injector Decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
+import { FavoriteDirective } from './favorite.directive';
 import { MediaItemFormComponent } from './media-item-form.component';
+import { MediaItemListComponent } from './media-item-list.component';
+import { MediaItemComponent } from './media-item.component';
+
+const lookupLists = {
+  mediums: ['Movies', 'Series', 'Documentary']
+};
 
 @NgModule({
   imports: [
@@ -20,6 +24,9 @@ import { MediaItemFormComponent } from './media-item-form.component';
     FavoriteDirective,
     CategoryListPipe,
     MediaItemFormComponent
+  ],
+  providers: [
+    {provide: 'lookupListsToken', useValue: lookupLists}
   ],
   bootstrap: [
     AppComponent
