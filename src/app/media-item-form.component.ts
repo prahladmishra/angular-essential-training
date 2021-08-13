a1815788d337896677798ee3227b69b2cb188406
Prahlad Mishra <prahlad.mishra@abbott.com>
2021-08-13 12:42:42 +0100

    Chapter concluded

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MediaItemService } from './media-item.service';
-
+import { lookupListToken } from './providers';
 @Component({
   selector: 'mw-media-item-form',
   templateUrl: './media-item-form.component.html',
@@ -13,7 +13,7 @@ export class MediaItemFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private mediaItemService: MediaItemService,
-    @Inject('lookupListToken') public lookupLists) {}
+    @Inject(lookupListToken) public lookupLists) {}
 
   ngOnInit() {
     this.form = this.formBuilder.group({
